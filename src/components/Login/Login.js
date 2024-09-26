d565ad9127fe3c0f13cbc11be3c70d20745d59c3
vishal_007 <davidjhaa4u@gmail.com>
2024-09-26 20:53:01 +0530

    refactored ui

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,136 +1,26 @@
-// import {useState} from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import styles from './Login.module.css'
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-// import { loginAdmin } from '../../api/api';
-
-
-// function Login() {
-//   const navigate = useNavigate();
-//   const notify = () => {
-//     toast.success("logged in successfully !", {
-//       position: "top-center"
-//     });
-//   }
-//   const notify2 = () => {
-//     toast.error("Invalid Credentials !", {
-//       position: "top-right"
-//     });
-//   }
-  
-//   const [formData, setFormData] = useState({ 
-//       email: "",
-//       password: "",
-//   });
-
-//   const handleChange = (event) => {
-//       setFormData({ ...formData, [event.target.name]: event.target.value });
-//   };
-
-//   const handleSubmit = async (event) => {
-//       let flag = true;
-
-//       if (formData.email.trim() === "") {
-//         setFormData(prevState => ({ ...prevState, email: "Invalid Email" }));
-//         flag = false;
-//       }
-//       if (formData.password.trim() === "") {
-//         setFormData(prevState => ({ ...prevState, password: "wrong Password" }));
-//         flag = false;
-//       }
-//       if(!flag){
-//         return;
-//       }
-
-//       const result = await loginAdmin(formData);
-//       if(result){
-//         notify();
-//         setTimeout(() => {
-//           navigate('/dashboard');
-//         }, 1500);
-//         return;
-//       }
-
-//       notify2();
-//       return;
-//   };
-
-//   const guestLogin = async (event) => {
-//     const result = await loginAdmin({email:'davidjhaa4u@gmail.com', password:'00000000'});
-//     if(result){
-//       notify();
-//       setTimeout(() => {
-//         navigate('/dashboard');
-//       }, 1500);
-//       return;
-//     }
-
-//     notify2();
-//     return;
-// };
-
-    
-//   return (
-//     <div className={styles.app}>
-//       <div className = {styles.parent}>
-//         <div className={styles.main}>
-//           <h1 className={styles.heading}>QUIZZIE</h1>
-//           <div className={styles.container}>
-//             <div onClick={ () => navigate('/signUp')} className={styles.signup}>Sign Up</div>
-//             <div className={styles.login}>Log In</div>
-//           </div>
-//           <div className={styles.form}>
-//             <div className={styles.field}>
-//               <label className={styles.label}>Email</label>
-//               <input
-//                   className={styles.input}
-//                   name="email"
-//                   style={{ color: formData.email === "Invalid Email" ? "red" : null }}
-//                   value={formData.email}
-//                   onChange={handleChange}
-//                   type={"email"}
-//               ></input>
-//             </div>
-
-//             <div className={styles.field}>
-//               <label className={styles.label}>Password</label>
-//               <input
-//                   className={styles.input}
-//                   name="password"
-//                   style={{ color: formData.password === "wrong Password" ? "red" : null }}
-//                   value={formData.password}
-//                   onChange={handleChange}
-//                   type={"text"}
-//               ></input>
-//             </div>
-//             <div>
-//               <button onClick={handleSubmit} className={styles.button}>
-//                   Login
-//               </button>
-//               <button onClick={guestLogin} className={styles.button}>
-//                 Guest Login
-//             </button>
-//             </div>
-//           </div>
-//         </div>
-//         <ToastContainer autoClose={1000}/>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Login
-
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './Login.module.css';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState } from "react";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import { IoLogoTwitter } from "react-icons/io5";
 import { loginAdmin } from '../../api/api';
 
-function Login() {
+
+
+const Login = () => {
   const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isGuestLoading, setIsGuestLoading] = useState(false);
+
   const notify = () => {
     toast.success('Logged in successfully!', { position: 'top-center' });
   };
@@ -138,58 +28,62 @@ function Login() {
     toast.error('Invalid Credentials!', { position: 'top-right' });
   };
 
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+  const loginwithgoogle = () => {
+    window.open("http://localhost:8000/auth/google/callback", "_self")
+  }
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .required("Please Enter your Email")
+      .email("Invalid Email"),
+    password: Yup.string()
+      .required("Please Enter your Password")
   });
 
-  const [isLoading, setIsLoading] = useState(false); // State for login button loader
-  const [isGuestLoading, setIsGuestLoading] = useState(false); // State for guest login loader
-
   const handleChange = (event) => {
+    setIsLoading(false);
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
-  const handleSubmit = async (event) => {
-    setIsLoading(true); // Start loading
+  const handleLogin = async () => {
+    try {
+      await validationSchema.validate(formData, { abortEarly: false });
+      setIsLoading(true);
 
-    let flag = true;
-    if (formData.email.trim() === '') {
-      setFormData((prevState) => ({ ...prevState, email: 'Invalid Email' }));
-      flag = false;
-    }
-    if (formData.password.trim() === '') {
-      setFormData((prevState) => ({ ...prevState, password: 'wrong Password' }));
-      flag = false;
-    }
-    if (!flag) {
-      setIsLoading(false); // Stop loading
-      return;
-    }
+      const result = await loginAdmin(formData);
+      setIsLoading(false); 
 
-    const result = await loginAdmin(formData);
-    setIsLoading(false); // Stop loading
-
-    if (result) {
-      notify();
-      setTimeout(() => {
-        navigate('/dashboard');
-      }, 1500);
-      return;
+      if (result) {
+        notify();
+        setTimeout(() => {
+          navigate('/dashboard');
+        }, 1500);
+        return;
+      }
     }
-
-    notify2();
+    catch (error) {
+      if (error.inner && error.inner.length > 0) {
+        toast.error(error.inner[0].message);
+      }
+      else {
+        toast.error(error.message);
+      }
+    }
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000);
   };
 
   const guestLogin = async (event) => {
-    setIsGuestLoading(true); // Start loading for guest login
+    setFormData({email:'',password:''});
+    setIsGuestLoading(true);
 
     const result = await loginAdmin({
       email: 'davidjhaa4u@gmail.com',
       password: '00000000',
     });
 
-    setIsGuestLoading(false); // Stop loading for guest login
+    setIsGuestLoading(false);
 
     if (result) {
       notify();
@@ -203,66 +97,84 @@ function Login() {
   };
 
   return (
-    <div className={styles.app}>
+    <div className={styles.main}>
       <div className={styles.parent}>
-        <div className={styles.main}>
-          <h1 className={styles.heading}>QUIZZIE</h1>
-          <div className={styles.container}>
-            <div onClick={() => navigate('/signUp')} className={styles.signup}>
-              Sign Up
+        <div className={styles.form}>
+          <div className={styles.content}>
+            <h2 className={styles.title}>Log In</h2>
+            <div className={styles.form__icons}>
+              <FcGoogle onClick={() => loginwithgoogle()} className={styles.formIcon} alt="icon1" />
+              <FaFacebook className={styles.formIcon2} alt="icon2" />
+              <IoLogoTwitter className={styles.formIcon3} alt="icon3" />
             </div>
-            <div className={styles.login}>Log In</div>
-          </div>
-          <div className={styles.form}>
-            <div className={styles.field}>
-              <label className={styles.label}>Email</label>
-              <input
-                className={styles.input}
-                name="email"
-                style={{
-                  color: formData.email === 'Invalid Email' ? 'red' : null,
-                }}
-                value={formData.email}
-                onChange={handleChange}
-                type="email"
-              />
-            </div>
-
-            <div className={styles.field}>
-              <label className={styles.label}>Password</label>
-              <input
-                className={styles.input}
-                name="password"
-                style={{
-                  color: formData.password === 'wrong Password' ? 'red' : null,
-                }}
-                value={formData.password}
-                onChange={handleChange}
-                type="text"
-              />
-            </div>
-            <div>
-              <button
-                onClick={handleSubmit}
-                className={styles.button}
-                disabled={isLoading} // Disable button while loading
+            <span className={styles.formSpan}>
+              or use email for login
+            </span>
+            <input
+              className={styles.formInput}
+              type="text"
+              placeholder="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <input
+              className={styles.formInput}
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <a href="" className={styles.forgetPassword}>
+              Forget password ?
+            </a>
+            <div className={styles.loginSignupbtn}>
+              <button 
+                className={styles.button} 
+                onClick={() => handleLogin()} 
+                disabled={isLoading} 
               >
-                {isLoading ? 'Loading...' : 'Login'} {/* Show loader */}
+              {isLoading ? (
+                <div className={styles.loader}></div>
+              ) : (
+                "Login"
+              )}
               </button>
               <button
-                onClick={guestLogin}
                 className={styles.button}
-                disabled={isGuestLoading} // Disable guest login button while loading
+                onClick={() => guestLogin()}
+                disabled={isLoading}
               >
-                {isGuestLoading ? 'Loading...' : 'Guest Login'} {/* Show loader */}
+                {isGuestLoading ? (
+                <div className={styles.loader}></div>
+              ) : (
+                "Guest login"
+              )}
               </button>
             </div>
           </div>
         </div>
-        <ToastContainer autoClose={1000} />
+        <div className={styles.switchContainer2}>
+          <div className={styles.bottomCircle}></div>
+          <div className={styles.upperCircle}></div>
+          <div className={styles.welcomeNote}>
+            <h2 className={styles.title}>Welcome Back!</h2>
+            <p className={styles.description}>
+              Great to see you again! <br />Let's pick up where you left off.
+            </p>
+            <button
+              className={styles.signInbutton}
+              onClick={() => navigate("/signUp")}
+            >
+              Sign up
+            </button>
+          </div>
+        </div>
       </div>
+      <ToastContainer autoClose={1500} />
     </div>
   );
-}
+};
 
 export default Login;
